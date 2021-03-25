60ea4954287415568b312503600c3d9bbdba123e
Dennis Prazak <prazak.dennis@gmail.com>
2021-03-25 14:58:06 +0100

    Restructure and add example program

---
+++ CMLParser.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMLParser.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
